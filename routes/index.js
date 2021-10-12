b31df091d7188b8b06e1ec0adb12d2b4f00bbe71
Jun <junchenli423@gmail.com>
2021-10-12 07:56:23 -0400

    ok

--- routes/index.js
+++ routes/index.js
@@ -25,23 +25,28 @@ router.get('/:username/:password/log-in', function(req,res,next){
 })
 router.post('/sign-up', function(req,res,next){
   console.log(req.body)
-  bcrypt.hash(req.body.password, 10, (err, hashed) => {
-    if(err){
-      res.json('failed')
+  User.find({username: req.body.username}, function(err, doc){
+    if(doc.length > 0){
+      return('Username Is Used')
     }
-    const user = new User({
-      username: req.body.username,
-      firstname: req.body.firstname,
-      email: req.body.email,
-      lastname: req.body.lastname,
-      password: hashed,
-    }).save(function(err){
+    bcrypt.hash(req.body.password, 10, (err, hashed) => {
       if(err){
-        console.log(err)
-        res.json('Bad Req')
-        return next(err)
+        res.json('failed')
       }
-      res.json('Good')
+      const user = new User({
+        username: req.body.username,
+        firstname: req.body.firstname,
+        email: req.body.email,
+        lastname: req.body.lastname,
+        password: hashed,
+      }).save(function(err){
+        if(err){
+          console.log(err)
+          res.json('Bad Req')
+          return next(err)
+        }
+        res.json('Good')
+      })
     })
   })
 })
