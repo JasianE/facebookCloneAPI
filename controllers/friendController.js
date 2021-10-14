e991a49dd9a9cbe0058a4f9965493d48a972b9ea
Jun <junchenli423@gmail.com>
2021-10-14 17:16:45 -0400

    funy

--- controllers/friendController.js
+++ controllers/friendController.js
@@ -4,13 +4,12 @@ exports.send = function(req,res,next){
     User.find({username: req.body.friendUsername}).exec(function(err, user){
         const person = user[0]
         const coolio = [...user[0].requests, req.body.user]
-        user[0].update({'requests': coolio}, (err) => {
+        user[0].update({'requests': coolio}, function(err){
             if(err){
-                console.log(err)
-                res.json('Failed')
-            } else{
-                res.json('Sucessful!')
+                res.json('Error:' + err)
+            } else {
+                res.json('Success!')
             }
-        }))
+        })
     })
 }
