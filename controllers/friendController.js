127874344e7e4b968cb860395140beb4a7127b40
Jun <junchenli423@gmail.com>
2021-10-14 12:44:56 -0400

    ok

--- controllers/friendController.js
+++ controllers/friendController.js
@@ -2,6 +2,15 @@ const User = require('../models/User')
 
 exports.send = function(req,res,next){
     User.find({username: req.body.friendUsername}).exec(function(err, user){
-        console.log(user)
+        const person = user[0]
+        const coolio = [...user[0].requests, req.body.user]
+        user[0].update({'requests': coolio}, (err) => {
+            if(err){
+                console.log(err)
+                res.json('Failed')
+            } else{
+                res.json('Sucessful!')
+            }
+        }))
     })
 }
