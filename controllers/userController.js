ee521a74d5408b540cdb2acf8f202049b1685d5b
Jun <junchenli423@gmail.com>
2021-10-15 15:52:21 -0400

    hok

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,11 @@
 const User = require('../models/User')
-
+//Finds User, returns user
+//Once displayed controller can send friend request 
+//Request uses home's current user and sends it to the requested friend user from state (stores new user in a state box that resets once the user leaves pages)
+//This request is then sent to the other user where they can either accept or reject
+//Once accepted use friendController.add
 exports.find = function(req,res,next){
     User.find({username: req.params.user}, function(err, user){
-        console.log(user)
+        console.log(user, req.param.user)
     })
 }
