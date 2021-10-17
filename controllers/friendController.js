6d92f9374a6a96beabbd58f9e7ddd62283fcf38f
Jun <junchenli423@gmail.com>
2021-10-17 14:15:55 -0400

    yesy

--- controllers/friendController.js
+++ controllers/friendController.js
@@ -35,9 +35,8 @@ exports.add = function(req,res,next){
     User.find({username: req.body.user}).exec(function(err, user){
         console.log(req.body.friend._id)
         const newFriends = [...user[0].friends, req.body.friend]
-        console.log(user[0].reqests)
-        const newRequests = user[0].requests.filter(item => item.equal(req.body.friend._id))
-        
+        const newRequests = user[0].requests.filter(item => item.toString() !== req.body.friend._id.toString())
+        console.log(newFriends, newRequests)
         user[0].update({'friends': newFriends, 'requests': newRequests}, function(err){
             if(err){
                 console.log(err)
