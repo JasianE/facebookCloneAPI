8bae3f66cdb747b48ed2c6f72aace1162bd50f5c
jun <junchenli423@gmail.com>
2021-10-21 18:43:32 -0400

    waht

--- controllers/friendController.js
+++ controllers/friendController.js
@@ -16,10 +16,9 @@ exports.send = function(req,res,next){
 exports.check = function(req,res,next){
     User.find({username: req.params.friend}).exec(function(err, use){
         User.find({username: req.params.user}).exec(function(err, bruh){
-            let result;
-            console.log(use[0].friends)
+            let result;)
             for(let i = 0; i < use[0].requests.length; i++){
-                if(use[0].requests[i]._id.equals(bruh[0]._id)){
+                if(use[0].requests[i]._id.equals(bruh[0]._id) || use[0].friends[i]._id.equals(bruh[0]._id)){
                     result = true
                     i = 99999999999999999999999999999999999999999
                 } 
