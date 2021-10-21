6107ec56a1a1bab6ef241be9b03616500d8ea53c
jun <junchenli423@gmail.com>
2021-10-21 19:51:31 -0400

    ok

--- controllers/friendController.js
+++ controllers/friendController.js
@@ -3,7 +3,7 @@ const User = require('../models/User')
 exports.send = function(req,res,next){
     User.find({username: req.body.friend.username}).exec(function(err, user){
         const person = user[0]
-        const coolio = [...user[0].requests, req.body.user]
+        const coolio = [...user[0].requests, req.body.user._id]
         user[0].update({'requests': coolio}, function(err){
             if(err){
                 res.json('Error:' + err)
@@ -20,7 +20,7 @@ exports.check = function(req,res,next){
             if(use[0].requests.length !== 0 || use[0].friends.length !== 0){
                 console.log(use[0].friends[0])
                 for(let i = 0; i < use[0].requests.length; i++){
-                    if(use[0].requests[i]._id.equals(bruh[0]._id) || use[0].friends[i]._id.equals(bruh[0]._id)){
+                    if(use[0].requests[i].equals(bruh[0]._id) || use[0].friends[i]._id.equals(bruh[0]._id)){
                         result = true
                         i = 99999999999999999999999999999999999999999
                     } 
