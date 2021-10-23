9df330849426f52191bd0c3617803b1d339ed801
jun <junchenli423@gmail.com>
2021-10-23 10:01:10 -0400

    ok

--- controllers/userController.js
+++ controllers/userController.js
@@ -48,8 +48,6 @@ exports.findPosts = function(req,res,next){
     sorts posts by date
     returns posts array
      */
-    for(let i = 0; i < req.params.friends.length; i++){
-        console.log(req.params.friends[i])
-    }
+    console.log(req.params.friend)
     console.log(JSON.parse(req.params.user))
 }
