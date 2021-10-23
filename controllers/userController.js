0858d499ec04fb833fc4218ae49c903b44fa6060
jun <junchenli423@gmail.com>
2021-10-23 18:45:11 -0400

    pk

--- controllers/userController.js
+++ controllers/userController.js
@@ -54,6 +54,7 @@ exports.findPosts = function(req,res,next){
         if(i === friends.length - 1){
             User.find({'_id': friends[i]}, function(err, user){
                 posts = [...posts, ...user[0].posts]
+                console.log(posts)
                 res.json(JSON.stringify(posts))
             })
         } else {
