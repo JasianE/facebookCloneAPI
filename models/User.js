802577fe4220c4edaec7919e471ea3a638be96ed
jun <junchenli423@gmail.com>
2021-10-27 09:39:58 -0400

    ok

--- models/User.js
+++ models/User.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const CommentSchema = new Schema({
-    message: {type: String, required: true},
-    likes: {type: Number, required: true}
+    message: {type: String, required: true}
 })
 const RequestSchema = new Schema({
     user: {type: String, required: true}
