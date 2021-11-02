1410b75f2925c6744fd025f6e2a5d4c0de4c0ea9
jun <junchenli423@gmail.com>
2021-11-02 07:52:34 -0400

    ok

--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,7 @@ function verifyToken(req,res,next){
       res.sendStatus(403)
   } else{
       const token = bearer.split(' ')[1]
+      console.log(token)
       req.token = token
       next()
   }
